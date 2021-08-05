3620200872fdc577d4440bd4ff5eee3c454c8ba3
ssanmap <sebastian.smp21@gmail.com>
2021-08-05 11:23:18 -0400

    fin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { ListadoComponent } from './components/listado/listado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    ListadoComponent
   ],
   imports: [
     BrowserModule
